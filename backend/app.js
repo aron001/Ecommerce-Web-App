8e15c84bc0ba2e3829f40083935cb3e6885a5ebc
Aron Hunde <aronhunde20@gmail.com>
2023-09-07 17:07:40 +0300

    sign up connect frot and back using axios

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const ErrorHandler = require("./utils/ErrorHandler");
 const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors= require("cors")
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/",express.static("uploads"))
+app.use(cors());
+app.use("/",express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
