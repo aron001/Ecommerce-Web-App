c3d4ed142e225c4ec84a462e703fe28ffd064b1b
Aron Hunde <aronhunde20@gmail.com>
2023-09-07 15:44:37 +0300

    signup

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,19 @@ const bodyParser = require("body-parser");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/",express.static("uploads"))
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-
+//config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "config/.env",
   });
 }
+// import routes
+const user = require("./controller/user");
+ 
+
+app.use("/api/v2/user", user);
 
 // it's for ErrorHandling
 app.use(ErrorHandler);
