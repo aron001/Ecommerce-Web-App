5294ff16606259a7e3c996b1004b08226e80f59e
Aron Hunde <aronhunde20@gmail.com>
2023-09-14 17:56:48 +0300

    first header part done

--- front/src/components/Layout/Header.jsx
+++ front/src/components/Layout/Header.jsx
@@ -4,10 +4,12 @@ import hululogooo from "../../Assets/hululogooo.png";
 import { Link } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { productData } from "../../static/data";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+
 function Header() {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchData, setSearchData] = useState(null);
-  
+
   const handleSearchChange = (e) => {
     const term = e.target.value;
     setSearchTerm(term);
@@ -16,7 +18,7 @@ function Header() {
       productData &&
       productData.filter((product) =>
         product.name.toLowerCase().includes(term.toLowerCase())
-  );
+      );
     setSearchData(filteredProducts);
   };
 
@@ -24,46 +26,57 @@ function Header() {
     <div className={`${styles.section} `}>
       <div className="hidden 800px:h-[50px] 800px:my-[20px] 800px:flex items-center justify-between">
         <div>
-        <Link to="/">
-          <img src={hululogooo} />
-        </Link></div>
-      
-      {/* search box */}
-      <div className="w-[50%] relative">
-        <input
-          type="text"
-          placeholder="Search Product..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
-        />
-        <AiOutlineSearch
-          size={30}
-          className="absolute right-2 top-1.5 cursor-pointer"
-        />
-        {searchData && searchData.length !== 0 ? (
-              <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
-                {searchData &&
-                  searchData.map((i, index) => {
-                    const d=i.name;
-                    const Product_name=d.replace(/\s+/g, "-")
-                    return (
-                      <Link to={`/product/${Product_name}`}>
-                        <div className="w-full flex items-start-py-3">
-                          <img
-                            src={i.image_Url[0]?.url}
-                            alt=""
-                            className="w-[40px] h-[40px] mr-[10px]"
-                          />
-                          <h1>{i.name}</h1>
-                        </div>
-                      </Link>
-                    );
-                  })}
-              </div>
-            ) : null}
+          <Link to="/">
+            <img src={hululogooo} />
+          </Link>
+        </div>
+
+        {/* search box */}
+        <div className="w-[50%] relative">
+          <input
+            type="text"
+            placeholder="Search Product..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+            className="h-[40px] w-full px-2 border-[#3957db] border-[2px] rounded-md"
+          />
+          <AiOutlineSearch
+            size={30}
+            className="absolute right-2 top-1.5 cursor-pointer"
+          />
+          {searchData && searchData.length !== 0 ? (
+            <div className="absolute min-h-[30vh] bg-slate-50 shadow-sm-2 z-[9] p-4">
+              {searchData &&
+                searchData.map((i, index) => {
+                  const d = i.name;
+                  const Product_name = d.replace(/\s+/g, "-");
+                  return (
+                    <Link to={`/product/${Product_name}`}>
+                      <div className="w-full flex items-start-py-3">
+                        <img
+                          src={i.image_Url[0]?.url}
+                          alt=""
+                          className="w-[40px] h-[40px] mr-[10px]"
+                        />
+                        <h1>{i.name}</h1>
+                      </div>
+                    </Link>
+                  );
+                })}
+            </div>
+          ) : null}
+        </div>
+        <div className={`${styles.button}`}>
+            <Link to="/seller">
+              <h1 className="text-[#fff] flex items-center">
+                Become Seller
+                <IoIosArrowForward className="ml-1" />
+              </h1>
+            </Link>
+          </div>
       </div>
-    </div></div>
+      
+    </div>
   );
 }
 
